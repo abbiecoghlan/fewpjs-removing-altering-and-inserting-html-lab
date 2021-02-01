1f5246d7273fd120cde3e3b1e659e6f1d48255e5
“Abbie <abbie.coghlan@gmail.com>
2021-02-01 11:45:56 -0600

    commit tree 0c336d398c13b0583a77c713f0b12bcd350e5aeb

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+// remove DOM node with id #main 
+document.querySelector("#main").remove();
+
+//make a 'newheader' variable that points to node 'h1#victory';
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "ABBIE is the champion"
